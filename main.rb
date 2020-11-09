5e04ea0e24a755ad2b8f70a30b977249305b1003
vagrant <shadmerhi@gmail.com>
2020-11-09 19:16:02 +0000

    First commit - project initialization

---
+++ main.rb
@@ -0,0 +1,9 @@
+puts "Enter a number "
+a = gets.chomp
+puts "Enter another number "
+b = gets.chomp
+
+puts "First input: #{a}"
+puts "First input: #{b}"
+
+puts "Numbers added: #{a.to_i + b.to_i}"
